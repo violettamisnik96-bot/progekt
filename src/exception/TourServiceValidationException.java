44c142d689b407e120f710169a5b2cea8324d3e7
Violetta <violettamisnik96@gmail.com>
2026-05-23 11:16:59 +0300

    Merge remote-tracking branch 'origin/main'

--- src/exception/TourServiceValidationException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class TourServiceValidationException extends Exception {
-    public TourServiceValidationException(String message) {
-        super(message);
-    }
-}
--- src/model/BookingStatus.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public enum BookingStatus {
-    PENDING,
-    CONFIRMED,
-    COMPLETED,
-    CANCELLED
-}
